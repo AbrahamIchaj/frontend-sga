3ab93a8ec6884b54591f7313c09301706fffdc41
Abraham Ichaj <abrahamichaj95@gmail.com>
2025-09-03 19:37:51 -0600

    Plantilla de login

---
+++ src/app/Login/Login.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './Login.component.html',
+  styleUrls: ['./Login.component.css']
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+  rememberMe: boolean = false;
+
+  onSubmit() {
+    console.log('Login submitted', { email: this.email, password: this.password, rememberMe: this.rememberMe });
+  }
+}
